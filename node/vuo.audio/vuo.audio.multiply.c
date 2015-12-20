d74fcbd112cfb52b5b48e6aa9f90a199ed3ab9fe
alcomposer <alcomposer@users.noreply.github.com>
2015-12-20 21:50:23 +1030

    added copyright info

--- node/vuo.audio/vuo.audio.multiply.c
+++ node/vuo.audio/vuo.audio.multiply.c
@@ -1,3 +1,12 @@
+/**
+ * @file
+ * vuo.audio.mix node implementation.
+ *
+ * @copyright Copyright © 2012–2014 Kosada Incorporated.
+ * This code may be modified and distributed under the terms of the MIT License.
+ * For more information, see http://vuo.org/license.
+ */
+
 #include "node.h"
 
 VuoModuleMetadata({
