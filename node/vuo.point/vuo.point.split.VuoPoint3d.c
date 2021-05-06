47411cc53c523b16fff1f0e4cf36bcfc5a6a11ef
Tara Khanal <tarakh99@gmail.com>
2021-05-06 18:35:18 -0400

    Add MM.ListTools.SplitXYZList as vuo.point.split.VuoPoint3d

---
+++ node/vuo.point/vuo.point.split.VuoPoint3d.c
@@ -0,0 +1,48 @@
+/**
+ * @file
+ * Sort by index implementation.
+ *
+ * @copyright Copyright © 2012–2018 Magneson.
+ * This code may be modified and distributed under the terms of the MIT License.
+ * For more information, see http://vuo.org/license.
+ */
+
+#include "node.h"
+
+VuoModuleMetadata({
+                      "title" : "Split Points (XYZ)",
+                      "keywords" : [ "sort", "indecies", "big", "high", "small", "most", ">", "<", "upper", "limit", "bound", "range" ],
+                      "version" : "2.1.0",
+                      "genericTypes" : {
+                          "VuoGenericType1" : {
+                              "defaultType" : "VuoReal",
+                              "compatibleTypes" : [ "VuoInteger", "VuoReal", "VuoPoint2d", "VuoPoint3d", "VuoPoint4d", "VuoImage", "VuoLayer" ]
+                          }
+                      },
+                      "node": {
+                          "exampleCompositions" : ["SplitXYZList.vuo"]
+                      }
+                  });
+
+void nodeEvent
+(
+        VuoInputData(VuoList_VuoPoint3d) points,
+        VuoOutputData(VuoList_VuoReal) x,
+        VuoOutputData(VuoList_VuoReal) y,
+        VuoOutputData(VuoList_VuoReal) z
+)
+
+{
+    *x = VuoListCreate_VuoReal();
+    *y = VuoListCreate_VuoReal();
+    *z = VuoListCreate_VuoReal();
+    VuoInteger listSize = VuoListGetCount_VuoPoint3d(points);
+
+    for(int i = 1; i <= listSize; i++)
+    {
+        VuoPoint3d point = VuoListGetValue_VuoPoint3d(points, i);
+        VuoListAppendValue_VuoReal(*x, point.x);
+        VuoListAppendValue_VuoReal(*y, point.y);
+        VuoListAppendValue_VuoReal(*z, point.z);
+    }
+}
