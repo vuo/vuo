157895c35509b7eaf62ce84e3e63092a40f3798e
Tara Khanal <tarakh99@gmail.com>
2021-05-06 22:07:13 -0400

    Add vuo.math.average.group.c based on MM.ListTools.AverageListItems to vuo.math

---
+++ node/vuo.math/vuo.math.average.group.c
@@ -0,0 +1,42 @@
+/**
+ * @file
+ * Average List Items implementation
+ *
+ * @copyright Copyright © 2012–2020 Magneson.
+ * This code may be modified and distributed under the terms of the MIT License.
+ * For more information, see http://vuo.org/license.
+ */
+
+#include "node.h"
+#include "math.h"
+
+VuoModuleMetadata({
+                      "title" : "Average List Items Groups",
+                      "keywords" : [ "magnitude", "bigger", "smaller", "some", "small", "most", ">", "<", "upper", "range" ],
+                      "version" : "1.0.0",
+                      "node": {
+                          "exampleCompositions" : [ "AverageListItems.vuo" ]
+                      }
+                  });
+
+void nodeEvent
+(
+        VuoInputData(VuoList_VuoReal) list,
+        VuoInputData(VuoInteger, {"default":1,"suggestedMin":1}) itemsPerGroup,
+        VuoOutputData(VuoList_VuoReal) averageValues
+)
+{
+    unsigned long listCount = VuoListGetCount_VuoReal(list);
+    *averageValues = VuoListCreate_VuoReal();
+
+    VuoList_VuoReal tempList = VuoListCreateWithCount_VuoReal(itemsPerGroup, 0.0);
+
+    for(int i = 1; i <= listCount; i = i + itemsPerGroup)
+    {
+        for(int j = 1; j <= VuoListGetCount_VuoReal(tempList); ++j)
+        {
+            VuoListSetValue_VuoReal(tempList, VuoListGetValue_VuoReal(list, i+ (j - 1)), j, false);
+        }
+        VuoListAppendValue_VuoReal(*averageValues, VuoReal_average(tempList));
+    }
+}
