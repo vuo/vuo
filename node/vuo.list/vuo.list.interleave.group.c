f9e3841fd2d28c204f36560af6bcb429fefac080
Jaymie Strecker <jstrecker@kosada.com>
2021-11-26 16:22:04 -0500

    Cleanup.

--- node/vuo.list/vuo.list.interleave.group.c
+++ node/vuo.list/vuo.list.interleave.group.c
@@ -1,6 +1,6 @@
 /**
  * @file
- * Combine Lists implementation.
+ * vuo.list.interleave.group node implementation.
  *
  * @copyright Copyright © 2012–2021 Kosada Incorporated.
  * This code may be modified and distributed under the terms of the MIT License.
@@ -9,52 +9,57 @@
 
 #include "node.h"
 
-    VuoModuleMetadata({
-                          "title" : "Interleave List Item Groups",
-                          "keywords" : [ "sort", "indecies", "big", "high", "small", "most", ">", "<", "upper", "limit", "bound", "range" ],
-                          "version" : "1.1.0",
-                          "genericTypes" : {
-                              "VuoGenericType1" : {
-                                  "defaultType" : "VuoReal",
-                                  "compatibleTypes" : [ "VuoInteger", "VuoReal", "VuoPoint2d", "VuoPoint3d", "VuoPoint4d", "VuoImage", "VuoLayer" ]
-                              }
-                          },
-                          "node": {
-                              "exampleCompositions" : ["CombineLists.vuo"]
-                          }
-                      });
+VuoModuleMetadata({
+	"title": "Interleave List Item Groups",
+	"keywords": [
+		"combine", "join", "together", "merge",
+		"alternate", "intersperse", "spread", "take turns", "rotate",
+		"cycle", "stride",
+		"array", "set", "sequence",
+		"items", "index", "indices",
+	],
+	"version": "1.0.0",
+	"node": {
+		"exampleCompositions": [ ],
+	},
+});
 
-    void nodeEvent
-    (
-            VuoInputData(VuoList_VuoGenericType1) list1,
-            VuoInputData(VuoList_VuoGenericType1) list2,
-            VuoInputData(VuoInteger, {"default":1,"suggestedMin":1}) itemPerGroup1,
-            VuoInputData(VuoInteger, {"default":1,"suggestedMin":1}) itemPerGroup2,
-            VuoOutputData(VuoList_VuoGenericType1) interleavedList
-    )
-
+void nodeEvent(
+	VuoInputData(VuoList_VuoGenericType1) list1,
+	VuoInputData(VuoInteger, {"name":"List 1 Items per Group","default":1,"suggestedMin":1}) list1ItemsPerGroup,
+	VuoInputData(VuoList_VuoGenericType1) list2,
+	VuoInputData(VuoInteger, {"name":"List 2 Items per Group","default":1,"suggestedMin":1}) list2ItemsPerGroup,
+	VuoOutputData(VuoList_VuoGenericType1) interleavedList)
 {
-    *interleavedList = VuoListCreate_VuoGenericType1();
+	unsigned long clampedList1ItemsPerGroup = MAX(1, list1ItemsPerGroup);
+	unsigned long clampedList2ItemsPerGroup = MAX(1, list2ItemsPerGroup);
 
-    unsigned long list1Count = floor((VuoListGetCount_VuoGenericType1(list1) / itemPerGroup1)) + itemPerGroup1;
-    unsigned long list2Count = floor((VuoListGetCount_VuoGenericType1(list2) / itemPerGroup2)) + itemPerGroup2;
+	unsigned long list1Count = VuoListGetCount_VuoGenericType1(list1);
+	unsigned long list2Count = VuoListGetCount_VuoGenericType1(list2);
 
-    for (unsigned long i = 0, j = 0; i <= list1Count && j <= list2Count; i+= itemPerGroup1, j+= itemPerGroup2)
-    {
-        if(i <= list1Count)
-        {
-            for(unsigned long k = 1; k <= itemPerGroup1; ++k)
-            {
-                VuoListAppendValue_VuoGenericType1(*interleavedList, VuoListGetData_VuoGenericType1(list1)[i + k - 1]);
-            }
-        }
+	unsigned long groupCount = MAX(ceilf(list1Count / (float)clampedList1ItemsPerGroup), ceilf(list2Count / (float)clampedList2ItemsPerGroup));
+	unsigned long outputCount = ((list1Count > 0 ? clampedList1ItemsPerGroup : 0)
+							   + (list2Count > 0 ? clampedList2ItemsPerGroup : 0)) * groupCount;
+	if (outputCount == 0)
+	{
+		*interleavedList = NULL;
+		return;
+	}
 
-        if(j <= list2Count)
-        {
-            for(unsigned long m = 1; m <= itemPerGroup2; ++m)
-            {
-                VuoListAppendValue_VuoGenericType1(*interleavedList, VuoListGetData_VuoGenericType1(list2)[j + m - 1]);
-            }
-        }
-    }
+	VuoGenericType1 *list1Data = VuoListGetData_VuoGenericType1(list1);
+	VuoGenericType1 *list2Data = VuoListGetData_VuoGenericType1(list2);
+
+	*interleavedList = VuoListCreateWithCount_VuoGenericType1(outputCount, VuoGenericType1_makeFromJson(NULL));
+	VuoGenericType1 *interleavedListData = VuoListGetData_VuoGenericType1(*interleavedList);
+	unsigned long m = 0;
+	for (unsigned long group = 0; group < groupCount; ++group)
+	{
+		if (list1Count)
+			for (unsigned long i = 0; i < clampedList1ItemsPerGroup; ++i)
+				interleavedListData[m++] = list1Data[MIN(group * clampedList1ItemsPerGroup + i, list1Count - 1)];
+
+		if (list2Count)
+			for (unsigned long i = 0; i < clampedList2ItemsPerGroup; ++i)
+				interleavedListData[m++] = list2Data[MIN(group * clampedList2ItemsPerGroup + i, list2Count - 1)];
+	}
 }
