98bc0b89f84d4f13a7aef7eac3939bd1c6f25bd4
Jaymie Strecker <jstrecker@kosada.com>
2021-11-26 16:26:19 -0500

    Cleanup.

--- node/vuo.list/vuo.list.wrap.c
+++ node/vuo.list/vuo.list.wrap.c
@@ -1,6 +1,6 @@
 /**
  * @file
- * Sort by index implementation.
+ * vuo.list.wrap node implementation.
  *
  * @copyright Copyright © 2012–2021 Kosada Incorporated.
  * This code may be modified and distributed under the terms of the MIT License.
@@ -10,34 +10,36 @@
 #include "node.h"
 
 VuoModuleMetadata({
-                      "title" : "Wrap List",
-                      "keywords" : [ "sort", "indecies", "big", "high", "small", "most", ">", "<", "upper", "limit", "bound", "range" ],
-                      "version" : "1.0.0",
-                      "genericTypes" : {
-                          "VuoGenericType1" : {
-                              "defaultType" : "VuoReal",
-                              "compatibleTypes" : [ "VuoInteger", "VuoReal", "VuoPoint2d", "VuoPoint3d", "VuoPoint4d", "VuoImage", "VuoLayer" ]
-                          }
-                      },
-                      "node": {
-                          "exampleCompositions" : ["ShiftList.vuo"]
-                      }
-                  });
+	"title": "Wrap List",
+	"keywords": [
+		"rotate", "shift", "loop", "cycle", "move", "rearrange", "displace", "repeat",
+		"array", "set", "sequence",
+		"items", "index", "indices",
+	],
+	"version": "1.0.0",
+	"node": {
+		"exampleCompositions": [ ],
+	},
+});
 
-void nodeEvent
-(
-        VuoInputData(VuoList_VuoGenericType1) list,
-        VuoInputData(VuoInteger, {"default":1,"suggestedMin":1}) startPosition,
-        VuoOutputData(VuoList_VuoGenericType1) wrappedList
-)
-
+void nodeEvent(
+	VuoInputData(VuoList_VuoGenericType1) list,
+	VuoInputData(VuoInteger, {"default":1,"suggestedMin":1}) startPosition,
+	VuoOutputData(VuoList_VuoGenericType1) wrappedList)
 {
-    *wrappedList = VuoListCreate_VuoGenericType1();
-    VuoInteger listSize = VuoListGetCount_VuoGenericType1(list);
+	VuoInteger listSize = VuoListGetCount_VuoGenericType1(list);
+	if (listSize < 1)
+	{
+		*wrappedList = NULL;
+		return;
+	}
 
-    for(int i = 0; i < listSize; i++)
-    {
-        VuoGenericType1 value = VuoListGetData_VuoGenericType1(list)[ ((startPosition - 1) + i) % listSize ];
-        VuoListAppendValue_VuoGenericType1(*wrappedList, value);
-    }
+	VuoGenericType1 *listData = VuoListGetData_VuoGenericType1(list);
+	*wrappedList = VuoListCreateWithCount_VuoGenericType1(listSize, VuoGenericType1_makeFromJson(NULL));
+	VuoGenericType1 *wrappedListData = VuoListGetData_VuoGenericType1(*wrappedList);
+	for (VuoInteger i = 0; i < listSize; ++i)
+	{
+		wrappedListData[i] = listData[VuoInteger_wrap(i + startPosition - 1, 0, listSize - 1)];
+		VuoGenericType1_retain(wrappedListData[i]);
+	}
 }
