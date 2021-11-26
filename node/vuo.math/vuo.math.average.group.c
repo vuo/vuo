c2b7afb4111d152dfb88f1f039d9aa08816545fe
Jaymie Strecker <jstrecker@kosada.com>
2021-11-26 16:18:05 -0500

    Cleanup.

--- node/vuo.math/vuo.math.average.group.c
+++ node/vuo.math/vuo.math.average.group.c
@@ -1,6 +1,6 @@
 /**
  * @file
- * Average List Items implementation
+ * vuo.math.average.group node implementation
  *
  * @copyright Copyright © 2012–2021 Kosada Incorporated.
  * This code may be modified and distributed under the terms of the MIT License.
@@ -11,32 +11,53 @@
 #include "math.h"
 
 VuoModuleMetadata({
-                      "title" : "Average List Items Groups",
-                      "keywords" : [ "magnitude", "bigger", "smaller", "some", "small", "most", ">", "<", "upper", "range" ],
-                      "version" : "1.0.0",
-                      "node": {
-                          "exampleCompositions" : [ "AverageListItems.vuo" ]
-                      }
-                  });
+	"title" : "Average List Item Groups",
+	"keywords": [
+		"mix", "combine", "mean", "midpoint", "middle",
+		"cycle", "stride"
+	],
+	"version" : "1.0.0",
+	"genericTypes": {
+		"VuoGenericType1": {
+			"defaultType": "VuoReal",
+			"compatibleTypes": [ "VuoInteger", "VuoReal", "VuoPoint2d", "VuoPoint3d", "VuoPoint4d" ],
+		},
+	},
+	"node": {
+		"exampleCompositions" : [ ]
+	}
+});
 
 void nodeEvent
 (
-        VuoInputData(VuoList_VuoReal) list,
-        VuoInputData(VuoInteger, {"default":1,"suggestedMin":1}) itemsPerGroup,
-        VuoOutputData(VuoList_VuoReal) averageValues
+	VuoInputData(VuoList_VuoGenericType1) list,
+	VuoInputData(VuoInteger, {"name":"Items per Group","default":1,"suggestedMin":1}) itemsPerGroup,
+	VuoOutputData(VuoList_VuoGenericType1) averageValues
 )
 {
-    unsigned long listCount = VuoListGetCount_VuoReal(list);
-    *averageValues = VuoListCreate_VuoReal();
+	unsigned long clampedItemsPerGroup = MAX(1, itemsPerGroup);
+	unsigned long listCount = VuoListGetCount_VuoGenericType1(list);
+	VuoGenericType1 *listData = VuoListGetData_VuoGenericType1(list);
+	*averageValues = VuoListCreate_VuoGenericType1();
 
-    VuoList_VuoReal tempList = VuoListCreateWithCount_VuoReal(itemsPerGroup, 0.0);
+	VuoList_VuoGenericType1 tempList = VuoListCreateWithCount_VuoGenericType1(clampedItemsPerGroup, 0);
+	VuoRetain(tempList);
 
-    for(int i = 1; i <= listCount; i = i + itemsPerGroup)
-    {
-        for(int j = 1; j <= VuoListGetCount_VuoReal(tempList); ++j)
-        {
-            VuoListSetValue_VuoReal(tempList, VuoListGetData_VuoReal(list)[i + (j - 1) -1], j, false);
-        }
-        VuoListAppendValue_VuoReal(*averageValues, VuoReal_average(tempList));
-    }
+	for (unsigned long i = 0; i < listCount; i += clampedItemsPerGroup)
+	{
+		// Average final partial group, if applicable.
+		unsigned long itemsInCurrentGroup = clampedItemsPerGroup;
+		if (listCount - i < clampedItemsPerGroup)
+		{
+			VuoListRemoveAll_VuoGenericType1(tempList);
+			itemsInCurrentGroup = listCount - i;
+		}
+
+		for (unsigned long j = 0; j < itemsInCurrentGroup; ++j)
+			VuoListSetValue_VuoGenericType1(tempList, listData[i + j], j + 1, true);
+
+		VuoListAppendValue_VuoGenericType1(*averageValues, VuoGenericType1_average(tempList));
+	}
+
+	VuoRelease(tempList);
 }
